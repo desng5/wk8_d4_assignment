d58a3fe20955e2c593dd6bed1c2400f2f7e85985
desng5 <desng5@gmail.com>
2023-06-15 21:30:03 -0700

    classes and methods

--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,150 @@
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
-type Item={
-    readonly id:string,
-    name:string,
-    price:number,
-    description:string
+//Items in the class & private attributes 
+class Item {
+  private id: string;
+  private name: string;
+  private price: number;
+  private description: string;
+
+  //Methods & Initializes the class attributes 
+  constructor(name: string, price: number, description: string) {
+    this.id = uuidv4();
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+
+  // Public Getters to access private attributes (outside class)
+  public getId(): string {
+    return this.id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getPrice(): number {
+    return this.price;
+  }
+
+  public getDescription(): string {
+    return this.description;
+  }
 }
 
-type User={
-    readonly id: string,
-    name: string,
-    age: number,
-    cart: Item[]
-}
+ // User & private attributes
+class User {
+  private id: string;
+  private name: string;
+  private age: number;
+  private cart: Item[];
 
-function createUser({name,age}:{name:string, age:number}){
-    
-    const user: User={
-        name,
-        age,
-        id:uuidv4(),
-        cart:[]
+  //Methods & Initalizes the class attributes
+  constructor(name: string, age: number) {
+    this.id = uuidv4();
+    this.name = name;
+    this.age = age;
+    this.cart = [];
+  }
+
+  // Public Getters to access private attributes
+  public getId(): string {
+    return this.id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getAge(): number {
+    return this.age;
+  }
+
+  public getCart(): Item[] {
+    return this.cart;
+  }
+
+  public addToCart(item: Item): void {
+    this.cart.push(item);
+  }
+
+  public removeFromCart(item: Item): void {
+    this.cart = this.cart.filter((i) => i.getId() !== item.getId());
+  }
+
+  public removeQuantityFromCart(item: Item, quantity: number): void {
+    for (let i = 0; i < quantity; i++) {
+      const index = this.cart.findIndex((cartItem) => cartItem.getId() === item.getId());
+      if (index !== -1) {
+        this.cart.splice(index, 1);
+      }
     }
-    return user
-}
+  }
 
-function createItem({name, price, description}:{name:string, price:number, description:string}){
-    const item:Item={
-        id:uuidv4(),
-        name,
-        price,
-        description
+  public cartTotal(): number {
+    let total = 0;
+    for (const item of this.cart) {
+      total += item.getPrice();
     }
-    return item
-}
+    return total;
+  }
 
-function addToCart(user:User, item:Item){
-    user.cart.push(item)
-}
-
-function removeFromCart(user:User, item:Item){
-    user.cart = user.cart.filter((i)=>i.id !== item.id)
-}
-
-function removeQuantityFromCart(user:User, item:Item, qty:number){
-    let i= 0
-    while (i<qty){
-        user.cart.splice(user.cart.findIndex((i)=> i.id == item.id), 1)
-        i++
+  public printCart(): void {
+    for (const item of this.cart) {
+      console.log(item.getName());
     }
+  }
 }
 
-function cartTotal(user:User){
-    let total=0;
-    for(let item of user.cart){
-        total+=item.price
-    }
-    return total
+class Shop {
+  private items: Item[];
+
+  //initializes i
+  constructor() {
+    this.items = [];
+    const roseBoquet = new Item("Rose Boquet", 75.99, "Large Boquet of Roses for celebration");
+    const lilyBoquet = new Item("Lily Boquet", 70.99, "Hawaiian Lillies for all occasions");
+    const tulipBoquet = new Item("Tulip Boquet", 40.99, "Vibrant and colorful display for all occasions");
+    this.items.push(roseBoquet, lilyBoquet, tulipBoquet);
+  }
+
+  //Public Getter to access class attributes
+  public getItems(): Item[] {
+    return this.items;
+  }
 }
 
-function printCart(user:User){
-    for(let item of user.cart){
-        console.log(item.name)
-    }
-}
+const shop = new Shop();
+const user = new User("Jade", 27);
 
-const user = createUser({name:"Jade", age:27})
-const roseBoquet = createItem({name: "Rose Boquet", price: 75.99, description: "Large Boquet of Roses for celebration"})
-const lilyBoquet = createItem({name:"Lily Boquet", price:70.99, description: "Hawaiian Lillies for all occasions"})
-const tulipBoquet = createItem({name:"Tulip Boquet", price:40.99, description:"Vibrant and cololful display for all occasions"})
+const items = shop.getItems();
+const roseBoquet = items[0];
+const lilyBoquet = items[1];
+const tulipBoquet = items[2];
 
+user.addToCart(roseBoquet);
+user.printCart();
+console.log(user.cartTotal());
 
-addToCart(user,roseBoquet)
-printCart(user)
-console.log(cartTotal(user))
+user.addToCart(lilyBoquet);
+user.addToCart(lilyBoquet);
+user.printCart();
+console.log(user.cartTotal());
 
+user.addToCart(tulipBoquet);
+user.addToCart(tulipBoquet);
+user.addToCart(tulipBoquet);
+user.printCart();
+console.log(user.cartTotal());
 
-addToCart(user,lilyBoquet)
-addToCart(user,lilyBoquet)
-printCart(user)
-console.log(cartTotal(user))
+user.removeFromCart(lilyBoquet);
+user.printCart();
+console.log(user.cartTotal(), "removed lilyBoquet");
 
-addToCart(user,tulipBoquet)
-addToCart(user,tulipBoquet)
-addToCart(user, tulipBoquet)
-printCart(user)
-console.log(cartTotal(user))
+console.log(user.cartTotal());
 
+user.removeQuantityFromCart(tulipBoquet, 2);
+user.printCart();
+console.log(user.cartTotal(), "2 Tulips removed");
 
-removeFromCart(user,lilyBoquet)
-printCart(user)
-
-console.log(cartTotal(user), "removed lilyBoquet")
-
-console.log(cartTotal(user))
-
-removeQuantityFromCart(user, tulipBoquet, 2)
-printCart(user)
-console.log(cartTotal(user), "2 Tulips removed")
